f84fc9ef1b5cf2fcbf992141b59f2e18022e8c72
Alexander Rubtsov <alex@rubtsov.su>
2024-02-21 21:44:09 +0600

    Replaced by the file from the class

--- 03-DataStructures-2/treap.py
+++ 03-DataStructures-2/treap.py
@@ -6,8 +6,8 @@ random.seed('codeforces!!111!')
 
 
 #For demo
-f = open("t0", "r")
-input = f.readline
+#f = open("t0", "r")
+#input = f.readline
 
 
 def read_int():
@@ -36,32 +36,35 @@ def write_array(array, **kwargs):
 
 class TreapNode:
     def __init__(self, key, prior):
-       self.key = key
-       self.prior = prior
-       self._left = None
-       self._right = None
-       self._parent = None
-       
+        self.key = key
+        self.prior = prior
+        self._left = None
+        self._right = None
+        self._parent = None
+        
+    # T.left = T_1
+    
     @property
     def left(self):
-         return self._left
-         
+        return self._left
+        
     @left.setter
-    def left(self, l): # self.left = l
-         self._left = l
-         if l is not None:
-             l._parent = self
-             
+    def left(self, l):
+        self._left = l
+        if l is not None:
+            l._parent = self
+            
     @property
     def right(self):
-         return self._right
-             
+        return self._right
+        
     @right.setter
     def right(self, r):
-         self._right = r
-         if r is not None:
-             r._parent = self
-             
+        self._right = r
+        if r is not None:
+            r._parent = self
+            
+    
     @property
     def parent(self):
         return self._parent
@@ -70,51 +73,49 @@ class TreapNode:
         return f"({self.key}, {self.prior})"
         
     def _print(self):
+        print(self, self.left, self.right)
         if self.left is not None:
             self.left._print()
-        print(self, end=" ")
         if self.right is not None:
             self.right._print()
-                
-       
-       
-class Treap: # Tree + Heap
-    def __init__(self, key=None, prior=None):
+        
+    
+class Treap:
+    def __init__(self, key=None, prior=None, node_class=TreapNode):   
+        self.nodes = []
+        self.node_class = node_class
         if key is None:
             assert prior is None
             self.root = None
         else:
-            self.root = TreapNode(key, prior)
-    
-    def insert(self, key, prior):        
-        node = TreapNode(key, prior)
+            self.root = node_class(key, prior)
+            self.nodes.append(root)
+            
+    def insert(self, key, prior):
+        node = self.node_class(key, prior)
+        self.nodes.append(node)
         if self.root is None:
             self.root = node
         else:
             self.root = self._insert(self.root, node)
-        
-    def _insert(self, t, node):
+            
+    def _insert(self, t, node: TreapNode):
         t1, t2 = self.split(t, node.key)
         t1 = self.merge(t1, node)
-        return self.merge(t1, t2)
-        
-        
-    def _remove(self, t, k):
-        pass # TBD next time
-        
-        
+        return self.merge(t1, t2)            
+                
     def split(self, t, k):
         if t is None:
-            return (None, None)
+            return None, None
         if k > t.key:
             t1, t2 = self.split(t.right, k)
             t.right = t1
-            return (t, t2)
-        else:
+            return t, t2
+        else: # k <= t.key
             t1, t2 = self.split(t.left, k)
             t.left = t2
-            return (t1, t)
-            
+            return t1, t
+    
     def merge(self, t1, t2):
         if t1 is None:
             return t2
@@ -127,81 +128,54 @@ class Treap: # Tree + Heap
         else:
             t2.left = self.merge(t1, t2.left)
             return t2
-          
+       
 
 class TreapNodeIndex(TreapNode):
-    def __init__(self, key, prior, idx):
-        super().__init__(key, prior)            
-        self.idx = idx
-    
-    def __str__(self):
-        return f"({self.key}, {self.prior}, {self.idx})"
-
+    def __init__(self, key, val, idx):
+        super().__init__(key, val)
+        self.idx = idx        
 
 class TreapIndex(Treap):
-      def __init__(self, key=None, prior=None, idx=None):
-          self.nodes = []
-          if key is None:
-              assert prior is None
-              assert idx is None
-              self.root = None
-          else:
-              self.root = TreapNodeIndex(key, prior, idx)
-              self.nodes.append(self.root)
+    def __init__(self, key=None, prior=None, idx=None, node_class=TreapNodeIndex):
+        self.nodes = []
+        self.node_class = node_class
+        if key is None:
+            assert prior is None
+            assert idx is None
+            self.root = None
+        else:
+            self.root = node_class(key, prior, idx)
+            self.nodes.append(root)
     
-      def insert(self, key, prior, idx):        
-          node = TreapNodeIndex(key, prior, idx)
-          self.nodes.append(node)
-          if self.root is None:
-              self.root = node
-          else:
-              self.root = self._insert(self.root, node)      
-     
-            
+    def insert(self, key, prior, idx):
+        node = self.node_class(key, prior, idx)
+        self.nodes.append(node)
+        if self.root is None:
+            self.root = node
+        else:
+            self.root = self._insert(self.root, node)
+        
+        
+
 n = read_int()
-
 t = TreapIndex()
-
-#for idx in range(n):
-#    key, prior = read_int_list()
-#    #print("=====")
-#    #print(key, prior)
-#
-#    t.insert(key, -prior, idx)        
-#    #print(t.root)
-#    #t.root._print()
-
-ikp = [ ]
+ikp = []
 for idx in range(n):
-   key, prior = read_int_list()
-   ikp.append((idx, key, prior)) 
-#list(enumerate([*(read_int_list()) for _ in range(n)])) # wrong :(
+    k, p = read_int_list()
+    ikp.append((idx, k, p))
 
-
-for idx, key, prior in sorted(ikp, key=lambda x: -x[2]):
-    t.insert(key, -prior, idx)       
-
-
+for idx, k, p in sorted(ikp, key=lambda _ikp: -_ikp[2]):
+    t.insert(k, -p, idx)
 
 print("YES")
-for node in sorted(t.nodes, key=lambda kpi: kpi.idx):
-    print( node.parent.idx + 1 if node.parent else 0, 
-           node.left.idx + 1 if node.left else 0,
-           node.right.idx + 1 if node.right else 0)
-
-
-exit(0)
-    
-print("\n%%%%%\n")    
-print(t.root, "\n")    
-t.root._print()
-
-
-       
-       
-       
-       
-       
+for node in sorted(t.nodes, key=lambda node: node.idx):
+    print(node.parent.idx + 1 if node.parent else 0,
+          node.left.idx + 1 if node.left else 0,
+          node.right.idx + 1 if node.right else 0)     
+    #print(node, node.left, node.right, node.idx)     
+        
+#print("t.root._print():")        
+#t.root._print()       
        
        
        
